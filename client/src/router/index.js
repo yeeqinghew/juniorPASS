6b28864a5ee94df9413f2cc25feda69b1e02eaa8
yeeqinghew <yeeqinghew@gmail.com>
2024-12-06 17:13:13 +0800

    fix: HTTPS instead of HTTP

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -80,7 +80,7 @@ export default () => {
       />
 
       <link
-        href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css"
         rel="stylesheet"
         type="text/css"
       ></link>
