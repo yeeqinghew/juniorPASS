aaf0fe44d5eb3e98a763974880cddf156a3ac5d3
yeeqinghew <yeeqinghew@gmail.com>
2025-11-28 16:08:19 +0800

    fix: scroll restoration on page load/navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Routers from "./router";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import "./App.css";
 import { UserProvider } from "./components/UserContext";
+
+// ScrollToTop component - Official React Router solution
+// Source: https://reactrouter.com/en/main/start/faq#how-do-i-scroll-to-the-top-when-the-route-changes
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <UserProvider>
         <Routers />
       </UserProvider>
